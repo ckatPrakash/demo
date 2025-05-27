f02f14f1978b5191bbcf373c7021530106881de0
ckatPrakash <ckatprakash@outlook.com>
2025-05-26 22:20:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,8 +94,8 @@ resource "aws_route_table_association" "rta" {
   route_table_id = aws_route_table.rt.id
 }
 
-resource "aws_security_group" "sg123" {
-  name        = "sg-demo123"
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
   description = "Allow SSH and http"
   vpc_id      = aws_vpc.vpc.id
 
@@ -125,7 +125,7 @@ resource "aws_instance" "ec2" {
   ami                         = "ami-0c02fb55956c7d316" 
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet.id
-  vpc_security_group_ids      = [aws_security_group.sg123.id]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
   #key_name                    = "your-key"  # Replace with your SSH key pair name
 
