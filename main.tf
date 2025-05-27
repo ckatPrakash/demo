dba4fe457fe2c11438fd4cd7bc9f7d555c8a6815
ckatPrakash <ckatprakash@outlook.com>
2025-05-26 22:11:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_instance" "ec2" {
   subnet_id                   = aws_subnet.subnet.id
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
-  key_name                    = "your-key"  # Replace with your SSH key pair name
+  #key_name                    = "your-key"  # Replace with your SSH key pair name
 
   tags = {
     Name = "application_server"
