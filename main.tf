2ea6a309171795582be544149b07a5abc7145fee
ckatPrakash <ckatprakash@outlook.com>
2025-05-26 21:54:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 provider "aws" {
 region = "us-east-1"
 }
@@ -51,4 +52,85 @@ resource "aws_db_instance" "mysql" {
 	tags = {
 		Name = "myRDS"
 	}
+}
+*/
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc-demo"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet-demo"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "sg" {
+  name        = "sg-demo"
+  description = "Allow SSH and http"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami                         = "ami-0c02fb55956c7d316" 
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet.id
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  associate_public_ip_address = true
+  key_name                    = "your-key"  # Replace with your SSH key pair name
+
+  tags = {
+    Name = "application_server"
+	Environment = "dev"
+  }
 }
